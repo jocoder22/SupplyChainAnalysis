7019c7afc63872fb354bb1c823e9a385e9818d91
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-19 23:14:04 -0400

    Update

--- supplyIntro.py
+++ supplyIntro.py
@@ -1,18 +1,22 @@
 import pandas as pd 
-from pulp import *
+# from pulp import LpProblem, LpMaximize, LpVariable, LpStatus
+import pulp as ch
 
 sp = '\n\n'
 
 
-model = LpProblem('Maximium Profit', LpMaximize)
-A = LpVariable("A", lowBound=0, cat="Integer")
-B = LpVariable("B", lowBound=0, cat="Integer")
+model = ch.LpProblem('Maximium Profit', ch.LpMaximize)
+A = ch.LpVariable("A", lowBound=0, cat="Integer")
+B = ch.LpVariable("B", lowBound=0, cat="Integer")
 
-model += 30*A + 50*B
+model += 10*A + 20*B
 
-model += 4.5*A + 2*B <= 280
+model += 2.5*A + 2*B <= 280
 model += 0.5*A + 1*B <= 110
 model += 2*B <= 80
 
 model.solve()
+print(f'Status: {ch.LpStatus[model.status]}')
+for v in model.variables():
+    print(f'{v.name} = {v.varValue}')
 
