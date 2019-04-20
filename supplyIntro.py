d5aacaca9e6b0da5750cb5e4df27d6e78244c2c1
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-19 22:52:41 -0400

    Update

--- supplyIntro.py
+++ supplyIntro.py
@@ -1,3 +1,16 @@
 import pandas as pd 
 from pulp import *
 
+sp = '\n\n'
+
+
+model = LpProblem('Maximium Profit', LpMaximize)
+A = LpVariable("A", lowBound=0, cat="Integer")
+B = LpVariable("B", lowBound=0, cat="Integer")
+
+model += 30*A + 50*B
+
+model += 4.5*A + 2*B <= 280
+model += 0.5*A + 1*B <= 110
+model += 2*B <= 80
+
