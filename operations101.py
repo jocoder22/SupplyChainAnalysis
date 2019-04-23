28c513e6da7bf815147beaed30afb14a72174f4e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-22 23:30:58 -0400

    Update

--- operations101.py
+++ operations101.py
@@ -6,3 +6,7 @@ import pulp as pul
 warehouse = ['NJ', 'NY', 'MD', 'IL']
 stores = ['pop', 'gog', 'lol', 'kol', 'mon']
 demands = [3000, 1200, 7000, 4100, 5600]
+
+demansdict = dict(zip(stores, demands))
+
+keys = [(w, s) for w in warehouse for s in stores]
