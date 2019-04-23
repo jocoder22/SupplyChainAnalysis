ef1e70b0f9ca4c5136d8bbec10238649258b9317
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-22 23:37:49 -0400

    Update

--- operations101.py
+++ operations101.py
@@ -6,7 +6,12 @@ import pulp as pul
 warehouse = ['NJ', 'NY', 'MD', 'IL']
 stores = ['pop', 'gog', 'lol', 'kol', 'mon']
 demands = [3000, 1200, 7000, 4100, 5600]
+cost = [300, 800, 100, 230, 90, 600, 240, 510]
 
 demansdict = dict(zip(stores, demands))
+costdict = dict(zip[(x, y) for x in warehouse for y in stores], cost))
 
 keys = [(w, s) for w in warehouse for s in stores]
+
+# Define objective function
+model += lp
