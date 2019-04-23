22c0be3aa2b7767191e90491c253694e103807df
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-22 23:47:11 -0400

    Update

--- operations101.py
+++ operations101.py
@@ -9,9 +9,11 @@ demands = [3000, 1200, 7000, 4100, 5600]
 cost = [300, 800, 100, 230, 90, 600, 240, 510]
 
 demansdict = dict(zip(stores, demands))
-costdict = dict(zip[(x, y) for x in warehouse for y in stores], cost)
+costdict = dict(zip([(x, y) for x in warehouse for y in stores], cost))
 
 keys = [(w, s) for w in warehouse for s in stores]
+vardict = pul.LpVariable.dicts('Storelist', keys, lowBound=0, cat='Integer')
 
 # Define objective function
-model += lp
+model += pul.lpSum([costdict[(w, s)] * vardict[(w, s)] 
+                for s in stores for w in warehouse])
