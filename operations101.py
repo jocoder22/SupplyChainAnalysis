21928a1fd97f4765e7b36bebfb0acab3658d822e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-23 00:07:20 -0400

    Update

--- operations101.py
+++ operations101.py
@@ -3,10 +3,10 @@ import pulp as pul
 
 # Warehouse distribution problem
 
-warehouse = ['NJ', 'NY', 'MD', 'IL']
+warehouse = ['NJ', 'NY', 'MD']
 stores = ['pop', 'gog', 'lol', 'kol', 'mon']
 demands = [3000, 1200, 7000, 4100, 5600]
-cost = [300, 800, 100, 230, 90, 600, 240, 510]
+cost = [300, 800, 100, 230, 90, 600, 240, 510, 892, 456, 125, 100, 840, 220, 234]
 
 demansdict = dict(zip(stores, demands))
 costdict = dict(zip([(x, y) for x in warehouse for y in stores], cost))
@@ -15,7 +15,7 @@ keys = [(w, s) for w in warehouse for s in stores]
 vardict = pul.LpVariable.dicts('Storelist', keys, lowBound=0, cat='Integer')
 
 # Define objective function
-model += pul.lpSum([costdict[(w, s)] * vardict[(w, s)] 
+model = pul.lpSum([costdict[(w, s)] * vardict[(w, s)] 
                 for s in stores for w in warehouse])
 
 for s in stores:
