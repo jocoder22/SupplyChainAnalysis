735e7cef6b61d08908aff006597760a518779d66
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-23 20:18:36 -0400

    Update

---
+++ operations102.py
@@ -0,0 +1,15 @@
+from pulp import *
+sp = '\n\n'
+
+iron = ['A', 'B', 'C', 'D']
+cost = [26, 30, 20, 8]
+lbs = [1000, 1000, 1000, 1]
+mag = [4.5, 5, 4, 1]
+sil = [40, 10, 6, 0]
+
+x = LpVariable.dicts('var_', iron, lowBound=0, cat='Continous')
+
+price = dict(zip(iron, cost))
+lbsdict = dict(zip(iron, lbs))
+magdict = dict(zip(iron, mag))
+sildict = dict(zip(iron, sil))
