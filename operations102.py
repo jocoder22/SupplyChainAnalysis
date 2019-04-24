cd833d73b34e4757ce2448a0aded936ee68a6b2f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-23 20:37:34 -0400

    Update

--- operations102.py
+++ operations102.py
@@ -2,10 +2,10 @@ from pulp import *
 sp = '\n\n'
 
 iron = ['A', 'B', 'C', 'D']
-cost = [26, 30, 20, 8]
-lbs = [1000, 1000, 1000, 1]
-mag = [4.5, 5, 4, 1]
-sil = [40, 10, 6, 0]
+cost = [26, 30, 20, 8.0]
+lbs = [1000, 1000, 1000, 1.0]
+mag = [4.5, 5.0, 4.0, 1.0]
+sil = [40, 10, 6.0, 0]
 
 x = LpVariable.dicts('var_', iron, lowBound=0, cat='Continous')
 
@@ -18,7 +18,7 @@ model = LpProblem('Max Profit', LpMinimize)
 model += lpSum([price[i] * x[i] for i in iron])
 
 model += lpSum([lbsdict[i] * x[i] for i in iron]) == 1000
-model += lpSum([lbsdict[i] * x[i] for i in iron]) >= 4.5
+model += lpSum([magdict[i] * x[i] for i in iron]) >= 4.5
 model += lpSum([sildict[i] * x[i] for i in iron]) >= 32.5
 model += lpSum([sildict[i] * x[i] for i in iron]) <= 55.0
 
