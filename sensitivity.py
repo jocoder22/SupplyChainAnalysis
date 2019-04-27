c019180fdf2a4ee3955dd3d6ee79e843f0610423
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-26 21:29:58 -0400

    Update

--- sensitivity.py
+++ sensitivity.py
@@ -47,12 +47,23 @@ def sensally():
     model1 = LpProblem('Max Profit', LpMaximize)
 
     # Define the objective function and add random variable
-    model1 += lpSum([(costdict[i] + nmv(0, 25)) * y[i] for i in house])
+    model += lpSum([(costdict[i] + nmv(0, 25)) * y[i] for i in house])
 
     # Define the constraints
-    model1 += lpSum([spacedict[i] * y[i] for i in house]) <= 50
-    model1 += lpSum([magdict[i] * y[i] for i in house]) <= 120
-    model1 += lpSum([gotdict[i] * y[i] for i in house]) <= 16
+    model += lpSum([spacedict[i] * y[i] for i in house]) <= 50
+    model += lpSum([magdict[i] * y[i] for i in house]) <= 120
+    model += lpSum([gotdict[i] * y[i] for i in house]) <= 16
 
     # Solve the model
-    model1.solve()
+    model.solve()
+
+    oo = {f'{i}':y[i].varValue for i in house}
+    oo['Objective'] = value(model.objective)
+
+    return oo
+
+output = []
+
+for t in range(100):
+    output.append(sensally())
+
